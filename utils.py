c7297939d13f641da4881834650feff79f1b4895
ashwinigit123 <ashwinitrale8@gmail.com>
2024-09-19 19:52:10 -0500

    deploy the changes on streamlit with secrets

--- utils.py
+++ utils.py
@@ -5,16 +5,25 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_community.chains.graph_qa.cypher import GraphCypherQAChain
 from langchain_community.graphs import Neo4jGraph
 
+
+import streamlit as st
+from neo4j import GraphDatabase
+
+
 load_dotenv()
 
-os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
-os.environ["NEO4J_URI"] = os.getenv("NEO4J_URI") or "bolt://localhost:7687"
-os.environ["NEO4J_USERNAME"] = os.getenv("NEO4J_USERNAME")
-os.environ["NEO4J_PASSWORD"] = os.getenv("NEO4J_PASSWORD") or "Neo4jgrad!23"
+# Try to get secrets from Streamlit, fall back to environment variables
+NEO4J_URI = st.secrets.get("NEO4J_URI") or os.getenv("NEO4J_URI")
+NEO4J_USERNAME = st.secrets.get("NEO4J_USERNAME") or os.getenv("NEO4J_USERNAME")
+NEO4J_PASSWORD = st.secrets.get("NEO4J_PASSWORD") or os.getenv("NEO4J_PASSWORD")
+GOOGLE_API_KEY = st.secrets.get("GOOGLE_API_KEY") or os.getenv("GOOGLE_API_KEY")
 
 def main(query):
-
-    graph = Neo4jGraph()
+    graph = Neo4jGraph(
+        url=NEO4J_URI,
+        username=NEO4J_USERNAME,
+        password=NEO4J_PASSWORD
+    )
  
     print(graph.schema)
 
